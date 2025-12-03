a00c666b4e837c81d5e75e8355ec0be7d726894c
Rishi Mukherjee <appleBananasaUCE@users.noreply.huggingface.co>
2025-12-03 01:43:46 +0000

    added the poke tool to this one as well

--- app.py
+++ app.py
@@ -9,14 +9,25 @@ from Gradio_UI import GradioUI
 
 # Below is an example of a tool that does nothing. Amaze us with your creativity !
 @tool
-def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
-    #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+def pokemon_info(name: str) -> str:
+    """Get Pokémon stats such as height, weight, type.
     Args:
-        arg1: the first argument
-        arg2: the second argument
+        name: The name of the pokemon.
     """
-    return "What magic will you build ?"
+    import requests
+    name_of_pokemon = name
+    url = f"https://pokeapi.co/api/v2/pokemon/{name_of_pokemon.lower()}"
+    res = requests.get(url)
+    if res.status_code != 200:
+        return "Pokémon not found."
+
+    data = res.json()
+    types = ", ".join([t["type"]["name"] for t in data["types"]])
+
+    return (
+        f"{data['name'].capitalize()}: "
+        f"Height {data['height']}, Weight {data['weight']}, Type(s): {types}"
+    )
 
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
@@ -55,7 +66,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+        tools=[final_answer, pokemon_info, get_current_time_in_timezone], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
